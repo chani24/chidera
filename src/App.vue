d06d2853f2022c01386e771a5c981a82a5f76015
Ani Chidera <achidera20@gmail.com>
2020-07-27 11:28:57 +0100

    first commit, WeJapa task 1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Chidera name="Ani Chidera James"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chidera from './components/Chidera.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Chidera
   }
 }
 </script>
---
+++ src/components/Chidera.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ name }}</h1>
+  </div>
+   
+</template>
+
+<script>
+export default {
+  name: 'Chidera',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
